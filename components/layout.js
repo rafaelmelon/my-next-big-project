aad61f319501febae6b303e1b03e222f9500fad4
Rafael Melon <rafael.melon@docplanner.com>
2023-09-19 15:17:21 +0200

    custom chrono

--- components/layout.js
+++ components/layout.js
@@ -6,8 +6,8 @@ import styles from './layout.module.css';
 import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
 
-const name = '[Your Name]';
-export const siteTitle = 'Next.js Sample Website';
+const name = 'My Next Big Project';
+export const siteTitle = 'My Next Big Project';
 
 export default function Layout({ children, home }) {
   return (
@@ -39,10 +39,10 @@ export default function Layout({ children, home }) {
           <>
             <Image
               priority
-              src="/images/profile.jpg"
+              src="/images/Hello.gif"
               className={utilStyles.borderCircle}
-              height={144}
-              width={144}
+              height={200}
+              width={200}
               alt={name}
             />
             <h1 className={utilStyles.heading2Xl}>{name}</h1>
@@ -52,7 +52,7 @@ export default function Layout({ children, home }) {
             <Link href="/">
               <Image
                 priority
-                src="/images/profile.jpg"
+                src="/images/Hello.gif"
                 className={utilStyles.borderCircle}
                 height={108}
                 width={108}
--- pages/index.js
+++ pages/index.js
@@ -1,36 +1,91 @@
 import Head from 'next/head';
+import { Chrono } from "react-chrono";
+
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
 import { getSortedPostsData } from '../lib/posts';
-import Link from 'next/link';
-import Date from '../components/date';
 
 export default function Home({ allPostsData }) {
+
+  const items = [{
+    title: "November 2020",
+    cardTitle: "Doctoralia adventure ",
+    cardDetailedText: "I join one of the best teams I've ever worked on 🚀",
+    media: {
+      type: "IMAGE",
+      source: {
+        url: "/images/FBBpUXuVEAshf4X.jpeg"
+      }
+    }
+  },
+  {
+    title: "November 2021",
+    cardTitle: "Work on my Sagrada Familia begins",
+    cardDetailedText: "Once the pandemic was coming to an end, I felt I was bored and bought an apartment that turned out to be a disaster. My motivation was very high so I started to rebuild it again. 🚧 Still WIP 🚧",
+    media: {
+      type: "IMAGE",
+      source: {
+        url: "/images/PXL_20230114_161730589.jpg"
+      }
+    }
+  },
+  {
+    title: "Many times during 2022",
+    cardTitle: "🍻 Doctoralia special meetings 🍻",
+    cardDetailedText: "There were many intense after-hours meetings, long, late-night sessions full of good times.",
+    media: {
+      type: "IMAGE",
+      source: {
+        url: "/images/giphy.gif"
+      }
+    }
+  },
+  {
+    title: "",
+    cardTitle: "'My Next Big Project' in 1, 2...",
+  },
+  {
+    title: "December 2023",
+    cardTitle: "A New Baby is Coming! 🍼",
+    cardDetailedText: ["It wasn't enough with my Sagrada Familia, I've started a new project with my partner. We are so happy!!","P.S. Any experienced recommendation is welcome."],
+    media: {
+      type: "IMAGE",
+      source: {
+        url: "/images/PXL_20230625_112727730.jpg"
+      }
+    }
+  }];
+
   return (
     <Layout home>
       <Head>
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
-        <p>
-          (This is a sample website - you’ll be building a site like this in{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
-      </section>
-      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Blog</h2>
-        <ul className={utilStyles.list}>
-          {allPostsData.map(({ id, date, title }) => (
-            <li className={utilStyles.listItem} key={id}>
-              <Link href={`/posts/${id}`}>{title}</Link>
-              <br />
-              <small className={utilStyles.lightText}>
-                <Date dateString={date} />
-              </small>
-            </li>
-          ))}
-        </ul>
+        <div style={{ width: "100%", height: "95vh" }}>
+          <Chrono 
+            items={items} 
+            mode="VERTICAL_ALTERNATING"
+            cardHeight={200}
+            mediaHeight={400}
+            hideControls
+            theme={{
+              primary: '#00C3A6',
+              secondary: '#00C3A6',
+              cardBgColor: 'white',
+              cardTitle: '#00C3A6',
+              titleColor: 'black',
+              titleColorActive: 'white',
+            }} 
+            fontSizes={{
+              cardSubtitle: '1rem',
+              cardText: '1rem',
+              cardTitle: '1.5rem',
+              title: '1rem',
+            }}
+          />
+        </div>
+        
       </section>
     </Layout>
   );
